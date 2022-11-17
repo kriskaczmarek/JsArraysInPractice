e985280a494604ec8442cb5d28f43a2f7cfea903
Krzysztof Kaczmarek <113107470+kriskaczmarek@users.noreply.github.com>
2022-11-17 08:51:41 +0100

    Update arrays.js
    Add descriptions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
+//method reduce in practice
+//joinin letters to obtain words, odd letters change to lower
 const arr = ["J", "U", "S", "T", "J", "O", "I", "N", ".", "I", "T"]
 const camelCaseArr = arr.reduce((a, b, c) => {
 	if (c % 2 !== 0) {
@@ -6,3 +8,4 @@ const camelCaseArr = arr.reduce((a, b, c) => {
 	return a + b
 }, "")
 console.log(camelCaseArr)
+//OUTPUT:  "JuStJoIn.iT"
